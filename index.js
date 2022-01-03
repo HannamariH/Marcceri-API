38cdef28fd230d6eaa2b6dfebd1773226cd2c779
Hannamari Heiniluoma <hannamari.h.heiniluoma@jyu.fi>
2022-01-03 14:44:46 +0200

    Run usemarcon to input file and return info concerning conversion success & titles of records

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 const Koa = require('koa')
 const Router = require('koa-router')
 const multer = require('@koa/multer')
-const { chunksToLinesAsync } = require('@rauschma/stringio');
-const { exec } = require('child_process');
+const { exec } = require('child_process')
+const fs = require("fs")
 
 const app = new Koa()
 const router = new Router()
@@ -15,6 +15,7 @@ const storage = multer.diskStorage({
         cb(null, '/usemarcon/uploads')
     },
     //TODO: tähän tarkistus, mikä on tiedostopääte/-tyyppi
+    //.mrc näkyy toimivan myös, vaikka pääte on xml
     filename: function (req, file, cb) {
         cb(null, "input.xml")
     }
@@ -25,58 +26,75 @@ const limits = {
     fileSize: 500 * 1024,//File Size Unit b
     files: 1//Number of documents
 }
-const upload = multer({storage,limits})
+const upload = multer({ storage, limits })
 
 const streamToString = async (readable) => {
-    let fullStdout = ""
-    for await (const line of chunksToLinesAsync(readable)) {
-        fullStdout += line    
+
+    const chunks = []
+
+    for await (const chunk of readable) {
+        chunks.push(Buffer.from(chunk))
     }
-    return fullStdout
+
+    return Buffer.concat(chunks).toString("utf-8")
+}
+
+const getTitles = () => {
+    const file = fs.readFileSync("/usemarcon/uploads/input.xml", "utf-8")
+
+    const titleFieldRegex = /<datafield tag="245"((.|\s)*?)<\/datafield>/g
+    const beforeTitleRegex = /<datafield tag="245"((.|\s)*?)<subfield code="a">/g
+    const afterTitleRegex = /<\/subfield>((.|\s)*?)$/g
+
+    const datafields = file.match(titleFieldRegex)
+
+    const titles = []    
+
+    for (let field of datafields) {
+        let strippedField = field.replace(beforeTitleRegex, "").replace(afterTitleRegex, "")
+        titles.push(strippedField)
+    }
+
+    return titles
 }
 
 //-----------routes--------------------------------
 
-router.post('/convert', upload.single('file'), async (ctx)=>{
+router.post('/convert', upload.single('file'), async (ctx) => {
 
-    console.log('ctx.request.body.ini:', ctx.request.body.ini)
+    //TODO: voiko tässä vielä poistaa edellisen input.xml:n? Tai onko tarvetta?
+    //TODO: tsekkaa, onko zip ja unzippaa
 
-    const output = exec(`/usemarcon/bin/usemarcon /usemarcon/${ctx.request.body.ini} /usemarcon/uploads/input.xml /usemarcon/output.xml`)
+    let output = ""
+    try {
+        output = exec(`/usemarcon/bin/usemarcon /usemarcon/${ctx.request.body.ini} /usemarcon/uploads/input.xml /usemarcon/output.xml`)
+        let result = await streamToString(output.stdout)
 
-    const result = await streamToString(output.stdout)
+        result = result.split("Conversion progress:")[1]
 
-    return ctx.body = {
-        data: result
-    }
+        //TODO: resultille parsimista, erroreista ja warningeista riippuen erilaiset responset
 
-    /*exec(`/usemarcon/bin/usemarcon /usemarcon/${ctx.request.body.ini} /usemarcon/uploads/input.xml /usemarcon/output.xml`, (error, stdout, stderr) => {
-    if (error) {
-        console.log("error: " + error.message)
+        const titles = getTitles()
+
         return ctx.body = {
-            data: ctx.file,
+            data: result,
+            titles: titles
+        }
+    } catch (error) {
+        console.log(error)
+        ctx.status = 500
+        return ctx.body = {
             error: error.message
         }
     }
-    if (stderr) {
-        console.log("stderr: " + stderr)
-        return ctx.body = {
-            data: ctx.file,
-            stderr: stderr
-        }
-    }
-    console.log("stdout: " + stdout)
-    return ctx.body = {
-        data: ctx.file,
-        stdout: stdout
-    }
-    })
 
-    return ctx.body = {
-        data: "viimeinen vara"
-    }*/
-    
 })
 
+//postaa output.xml-tiedoston tietueet Kohaan, yksi kerrallaan
+router.post("/toKoha", async (ctx) => {
+    //TODO: tarkista, että on olemassa output.xml, josta voi poimia tietueet lähetettäväksi
+    //(miten tarkistetaan, että output.xml on tuore?)
+})
 
 
 app.use(router.routes()).use(router.allowedMethods())
